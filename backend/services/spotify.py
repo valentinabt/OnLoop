14a270a503fea0d375fe0c05b159ee9f53666fe0
valentinabt <vvalentinabelmonte@gmail.com>
2026-04-28 21:37:27 -0300

    fix: add redis password variable

--- backend/services/spotify.py
+++ backend/services/spotify.py
@@ -1,9 +1,9 @@
 import requests
 import redis
 import uuid
-from backend.config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REDIS_HOST, REDIS_PORT
+from backend.config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, password= REDIS_PASSWORD)
 
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
