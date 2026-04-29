895e2510bf879864f0d3b8989c5d8e517ae91192
valentinabt <vvalentinabelmonte@gmail.com>
2026-04-29 20:48:22 -0300

    Debug: prints

--- backend/main.py
+++ backend/main.py
@@ -51,6 +51,8 @@ def login():
 
 @app.get("/callback")
 def callback(code: str = None, error: str = None,state: str = None, state_cookie: str = Cookie(default=None)):
+    print("STATE FROM SPOTIFY:", state)
+    print("STATE COOKIE:", oauth_state)
     if error:
         return RedirectResponse(f"{FRONTEND_URL}")
    
